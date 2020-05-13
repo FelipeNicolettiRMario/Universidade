bc427e4c2d86a72ab87513357213cc1232be3725
Felipe Nicoletti Reis Mario <46199467+FelipeNicolettiRMario@users.noreply.github.com>
2020-05-13 20:46:18 -0300

    Initial Classes

---
+++ src/alunos/Prova.java
@@ -0,0 +1,15 @@
+package alunos;
+
+public class Prova {
+	int matricula;
+	String nome;
+	String curso;
+	
+	public Prova(int matricula,String nome,String curso) {
+		this.matricula = matricula;
+		this.nome = nome;
+		this.curso = curso;
+	}
+
+}
+
